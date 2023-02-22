6fe5bde1f62421e12502881efcd38823409176be
idilipsarker <dilipsarker719@gmail.com>
2023-02-22 14:07:06 +0600

    basic folder structure and navbar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEmLjq4d4LWd0iWy6A6tBUeLbdWQoP0sQ",
+  authDomain: "dragon-news-e5956.firebaseapp.com",
+  projectId: "dragon-news-e5956",
+  storageBucket: "dragon-news-e5956.appspot.com",
+  messagingSenderId: "976072138866",
+  appId: "1:976072138866:web:130c135c0d3fb97cb762eb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Shared/RightSideNav/RightSideNav.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import BrandCarousel from "../BrandCarousel/BrandCarousel";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaWhatsapp,
+  FaTwitch,
+} from "react-icons/fa";
+
+const RightSideNav = () => {
+  return (
+    <div>
+      <ButtonGroup vertical>
+        <Button variant="outline-primary">
+          <FaGoogle></FaGoogle> Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          <FaGithub></FaGithub> Login with Github
+        </Button>
+      </ButtonGroup>
+      <div className="mt-2">
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook></FaFacebook> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter></FaTwitter> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp></FaWhatsapp> Whatsapp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitch></FaTwitch> Twitch
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <BrandCarousel></BrandCarousel>
+      </div>
+    </div>
+  );
+};
+
+export default RightSideNav;
